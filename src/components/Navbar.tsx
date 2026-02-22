2c952ef591205b67340e519d64ac8d07913baedf
csokunmuhammed <coskunmustafa744@gmail.com>
2026-02-22 21:46:03 +0300

    feat: add smooth scroll to top on navbar logo click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,16 @@ export default function Navbar() {
                 }`}
         >
             <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
-                <Link href="/" className="flex items-center">
+                <Link
+                    href="/"
+                    className="flex items-center cursor-pointer"
+                    onClick={(e) => {
+                        if (window.location.pathname === "/") {
+                            e.preventDefault();
+                            window.scrollTo({ top: 0, behavior: "smooth" });
+                        }
+                    }}
+                >
                     <Logo
                         scrolled={scrolled}
                         className="h-14 md:h-20 w-auto"
